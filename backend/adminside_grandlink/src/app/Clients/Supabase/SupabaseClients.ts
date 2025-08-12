77812d32fc3f41878d88fe69e50ba909ad1506b0
JOHN <johnezrabugao811@gmail.com>
2025-08-12 12:59:41 +0800

    Update

--- backend/adminside_grandlink/src/app/Clients/Supabase/SupabaseClients.ts
+++ backend/adminside_grandlink/src/app/Clients/Supabase/SupabaseClients.ts
@@ -1,6 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co';
-const supabaseKey = 'YOUR_PUBLIC_ANON_KEY';
+const supabaseUrl = 'https://gijnybivawnsilzqegik.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdpam55Yml2YXduc2lsenFlZ2lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyODAyMjUsImV4cCI6MjA2OTg1NjIyNX0.-gO8DcuK9-Q7nQmHRGnKJX3j8W0xHk925KlALBth1gU';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export async function getOrders() {
+  const { data, error } = await supabase.from("orders").select("*");
+  if (error) {
+    throw new Error(error.message);
+  }
+  return data;
+}
