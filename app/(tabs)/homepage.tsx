642cc81e9dd7200b9a60f6e34d134a437685db67
Evbread <itsyourkurt25@gmail.com>
2025-09-04 01:00:02 +0800

    Added few more in homepage

--- app/(tabs)/homepage.tsx
+++ app/(tabs)/homepage.tsx
@@ -1,5 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { View, Image, StyleSheet, TouchableOpacity, Text, ScrollView } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Image, StyleSheet, TextInput as RNTextInput, TouchableOpacity, Text, ScrollView, Button} from 'react-native';
+import { Video as ExpoVideo} from "expo-av";
+import { blue } from 'react-native-reanimated/lib/typescript/Colors';
 
 const images = [
   require('@/assets/images/homeimage1.png'),
@@ -9,8 +12,9 @@ const images = [
   require('@/assets/images/homeimage5.png'),
 ];
 
-export default function Homescreen() {
-  const [index, setIndex] = useState(0);
+export default function Homescreen() {  
+    const [index, setIndex] = useState(0);
+    const Video: any = ExpoVideo;
 
   const nextImage = () => setIndex((prev) => (prev + 1) % images.length);
   const prevImage = () => setIndex((prev) => (prev - 1 + images.length) % images.length);
@@ -23,6 +27,7 @@ export default function Homescreen() {
   }, []);
 
   return (
+    <View style={styles.whitebackground}>
     <View style={{ flex: 1 }}>
       <ScrollView style={{ flex: 1 }}>
         <View style={styles.upperBar}>
@@ -51,6 +56,7 @@ export default function Homescreen() {
           <View style={styles.qualityDot} />
           <Text style={styles.qualityText}>High Quality, Long Lasting Performance</Text>
         </View>
+
 
         <TouchableOpacity style={styles.imageButton}>
           <Image
@@ -107,14 +113,94 @@ export default function Homescreen() {
             </View>
           </View>
         </TouchableOpacity>
-        <View style={styles.blueBox}>
-            <View style={{ flexDirection: 'row', width: '100%', paddingLeft: 30, paddingTop: 20 }}>
+        
+        <View style={styles.blueBoxB}>
+          <View style={{ flexDirection: "row", paddingLeft: 10, paddingTop: 20, alignItems: "center" }}>
             <Text style={styles.featuredText}>Featured Projects</Text>
             <TouchableOpacity style={styles.viewMoreProjectButton}>
-                <Text style={styles.viewMoreProjectButtonText}>View More Projects</Text>
+              <Text style={styles.viewMoreProjectButtonText}>View More Projects</Text>
             </TouchableOpacity>
-            </View>
+          </View>
+          <Video
+            source={require("@/assets/videos/testvideo.mp4")}
+            useNativeControls
+            resizeMode="cover"
+            style={{
+              width: "95%",
+              height: 200,
+              alignSelf: "center",
+              marginVertical: 15,
+              borderRadius: 12,
+              backgroundColor: "black",
+            }}
+          />
+          <ThemedText style={{ color: "white", fontSize: 18, fontWeight: 'bold', textAlign: "center", marginBottom: 4 }}>
+            Mikey Bustos ft. Grand East Products
+          </ThemedText>
+          <ThemedText style={{ color: "white", fontSize: 10, textAlign: "left", marginBottom: 3 }}>
+            Join Mikey Bustos on an exciting shopping spree vlog, where he showcases our product as his top choice for stylish
+            glass, doors, and windows. Discover his tips and insights as he transform his space with these functional and aesthetic
+            upgrades! Credit to: Mikey Bustos.
+          </ThemedText>
+
+          <Video
+            source={require("@/assets/videos/testvideo.mp4")}
+            useNativeControls
+            resizeMode="cover"
+            style={{
+              width: "95%",
+              height: 200,
+              alignSelf: "center",
+              marginVertical: 15,
+              borderRadius: 12,
+              backgroundColor: "black",
+            }}
+          />
+
+          <ThemedText style={{ color: "white", fontSize: 18, fontWeight: 'bold', textAlign: "center", marginBottom: 4 }}>
+            Solenn Heusaff ft. Grand East Products
+          </ThemedText>
+          <ThemedText style={{ color: "white", fontSize: 10, textAlign: "left", marginBottom: 3 }}>
+            Solenn Heusaff invites us into her beautifully curated home, where our products seamlessly blend with her style. 
+            Each piece reflects her unique taste and attention to detail, enchancing the warmth and sophistication of her living space. 
+            Credit to: Solenn Heusaff.
+          </ThemedText>
+
+            <View style={{
+                flexDirection: 'row',
+                width: '120%',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                gap: 10, // Adds a 10px gap between items
+                paddingHorizontal: 20,
+                paddingBottom: 20,
+                paddingTop: 20
+              }}>
+                <TouchableOpacity style={styles.imageButtonB}>
+                  <Image
+                    source={require('@/assets/images/featuredprod1.png')}
+                    style={styles.imageButtonImageB}
+                    resizeMode="cover"
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.imageButtonB}>
+                  <Image
+                    source={require('@/assets/images/featuredprod2.png')}
+                    style={styles.imageButtonImageB}
+                    resizeMode="cover"
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.imageButtonB}>
+                  <Image
+                    source={require('@/assets/images/featuredprod3.png')}
+                    style={styles.imageButtonImageB}
+                    resizeMode="cover"
+                  />
+                </TouchableOpacity>
+              </View>
+
         </View>
+
       </ScrollView>
       <View style={styles.redLowerBar}>
         <View style={styles.sideIconsContainer}>
@@ -158,30 +244,45 @@ export default function Homescreen() {
         </View>
       </View>
     </View>
+
+    
+    </View>
+    
     
     
   );
 }
 
 const styles = StyleSheet.create({
+whitebackground: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+},
     upperBar: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     padding: 15,
-    backgroundColor: '#fff',
+    backgroundColor: '#fdfdfdff',
   },
   logo: {
     height: 70,
     width: 200,
   },
   blueBox: {
-    height: '20%',
+    height: '12%',
     backgroundColor: '#1c202aff',
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
+  },
+  blueBoxB: {
+       backgroundColor: '#1c202aff',
+        width: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 20,
   },
   arrow: {
     padding: 10,
@@ -214,12 +315,24 @@ const styles = StyleSheet.create({
   imageButton: {
     marginTop: 7,
     marginHorizontal: 16,
-    borderRadius: 8,
+    borderRadius: 10,
     overflow: 'hidden',
     height: 180,
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 16,
+    color: '#fff',
+  },
+  imageButtonB: {
+  flex: 1, 
+  width: '40%',
+  height: 190,
+  borderRadius: 5,
+  overflow: 'hidden',
+},
+  imageButtonImageB: {
+    width: '100%',
+  height: '100%',
   },
   imageButtonImage: {
     width: '100%',
@@ -232,7 +345,7 @@ const styles = StyleSheet.create({
     height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.25)',
+    backgroundColor: 'rgba(0, 0, 0, 0.25)',
   },
   imageButtonTitle: {
     color: '#fff',
@@ -323,5 +436,36 @@ viewMoreProjectButtonText: {
   fontWeight: 'bold',
   fontSize: 10,
 },
-
+    inquireTitle: {
+        color: '#722727ff',
+        fontSize: 30, 
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 20,
+        marginBottom: 10,
+        
+    },
+    inquireSubText: { 
+        color: '#2e2e2eff',
+        fontSize: 14,
+        textAlign: 'center',
+        marginTop: 7,
+        marginBottom: 20,   
+        paddingHorizontal: 20,
+    },
+    inputLabel: {
+    marginBottom: 5,
+    fontWeight: '600',
+    color: '#080808ff',
+    },
+    inputWrapper: {
+      borderWidth: 1,
+      borderColor: '#323232ff',
+      borderRadius: 5,
+    },
+    input: {
+    paddingVertical: 6,
+    paddingHorizontal: 10,
+    fontSize: 13,
+    },
 });
