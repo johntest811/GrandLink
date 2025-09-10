7e5a57e120d018c85072018a68e5d46409869da7
JohnEzraBugao <88995683+johntest811@users.noreply.github.com>
2025-09-10 21:15:50 +0800

    Delete mobile directory

--- mobile/grandlink_mobile/app/(tabs)/login.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import LoginScreen from '../login';
-
-export default function LoginTab() {
-  return <LoginScreen />;
-}
--- mobile/grandlink_mobile/app/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  // Redirect to the login page when the app starts
-  return <Redirect href="/login" />;
-}
