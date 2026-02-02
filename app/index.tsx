ea095f4e90edb29be0b09b9649565b7bc7b5ba67
Evbread <itsyourkurt25@gmail.com>
2026-02-02 23:07:49 +0800

    Fix duplicates and AR Cam Updates 2/2/2026

--- app/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from 'expo-router';
-import React from 'react';
-
-export default function Index() {
-  // Redirect to the homepage when the app starts
-  // Users can browse without logging in
-  return <Redirect href="/login" />;
-}
--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add .hdr files as assets so they can be required/imported
-config.resolver.assetExts.push('hdr');
-
-module.exports = config;
